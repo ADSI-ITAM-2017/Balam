234155f846aba50eea37e4329b79bdd2c73f5a86
davidalc2012 <davidalc2012@gmail.com>
2017-05-01 22:45:08 +0000

    Adición de controlador de usuario

--- Aplicación/config/routes.rb
+++ Aplicación/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   root 'startup#index'
   
-  get  '/ayuda',    to: 'startup#ayuda'
   
+  get  '/ayuda',    to: 'startup#ayuda'
+  get '/registrate', to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
