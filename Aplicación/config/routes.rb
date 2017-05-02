2a1342f20abf6b2883419524bc2d08c340b3c2dd
davidalc2012 <davidalc2012@gmail.com>
2017-05-02 08:27:29 +0000

    Formulario de registro

--- Aplicación/config/routes.rb
+++ Aplicación/config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get  '/ayuda',    to: 'startup#ayuda'
   get '/registrate', to: 'users#new'
 
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
