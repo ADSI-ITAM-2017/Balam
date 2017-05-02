36e4fd326757595ad99dccc13352d33406b3648e
davidalc2012 <davidalc2012@gmail.com>
2017-05-02 07:29:50 +0000

    Validación de contraseña y seguridad

--- Aplicación/db/schema.rb
+++ Aplicación/db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502070420) do
+ActiveRecord::Schema.define(version: 20170502070854) do
 
   create_table "users", force: :cascade do |t|
     t.string   "nombre"
     t.string   "email"
     t.date     "fecha"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
